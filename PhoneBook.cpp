841e433c303c7040199f6290648974013ebedbea
Baljeet Singh <prince.singh4000@gmail.com>
2021-06-10 02:12:51 +0530

    Update PhoneBook.cpp

--- PhoneBook.cpp
+++ PhoneBook.cpp
@@ -1,10 +1,33 @@
-#include<iostream>
-#include<conio.h>
-#include<stdlib.h>
-
+#include <bits/stdc++.h>
+#include <fstream>
 using namespace std;
-
-string name[30];
-string mobileNo[10];
-string email[30];
-string address[30];
+int main()
+{
+  ofstream outFile;
+  outFile.open("Contacts.txt", ios::app);
+  string quit = "y";
+  string name;
+  string number;
+  string email;
+  string address;
+  while (quit != "n") 
+  {
+    cout << "Full Name : ";
+    getline(cin, name);
+    
+    cout << "Mobile Number : ";
+    getline(cin, number);
+    
+    cout << "E-mail : ";
+    getline(cin, email);
+    
+    cout << "Address : ";
+    getline(cin, address);
+    
+	outFile << name <<" -> "<<number<<" -> "<<email<<" -> "<<address<<endl;
+    cout << "Add New Contact (y/n)? ";
+    getline(cin, quit);
+  }
+  outFile.close();
+  return 0;
+}

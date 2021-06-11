970824473b3e30a0ba20d03649beb769b8369f80
Baljeet Singh <prince.singh4000@gmail.com>
2021-06-12 02:49:45 +0530

    Update PhoneBook.cpp

--- PhoneBook.cpp
+++ PhoneBook.cpp
@@ -3,6 +3,54 @@
 
 using namespace std;
 
+class encdec
+{
+	int key;
+	string fle="Contacts.txt";
+	char c;
+	public:
+		void encrypt();
+		void decrypt();
+};
+void encdec::encrypt()
+{
+	cout<<"key: ";
+	cin>>key;
+	ifstream fin;
+	ofstream fout;
+	fin.open(fle, ios::binary);
+	fle = "EncCont.txt";
+	fout.open(fle, ios::binary);
+	while(!fin.eof())
+	{
+		fin>>noskipws>>c;
+		int temp=(c+key);
+		fout<<(char)temp;
+	}
+	fin.close();
+	fout.close();
+	
+}
+
+void encdec::decrypt()
+{
+	cout<<"key: ";
+	cin>>key;
+	ifstream fin;
+	ofstream fout;
+	fin.open("EncCont.txt", ios::binary);
+	fle = "DecCont.txt";
+	fout.open(fle, ios::binary);
+	while(!fin.eof())
+	{
+		fin>>noskipws>>c;
+		int temp=(c-key);
+		fout<<(char)temp;
+	}
+	fin.close();
+	fout.close();
+}
+
 void addcont()
 {
 	string name;
@@ -11,8 +59,8 @@ void addcont()
     string address;
 	string quit = "y";
 	ofstream File;
-    File.open("Contacts.txt",ios::app);
-    while(quit != "n" || "N")
+    File.open("Contacts.txt", ios::app);
+    while(quit!="n")
 	{
 		cout << "Full Name : ";
         getline(cin, name);
@@ -28,7 +76,8 @@ void addcont()
     
 	    File << name <<" -> "<<number<<" -> "<<email<<" -> "<<address<<endl;
         cout << "Add New Contact (y/n)? ";
-        getline(cin, quit);
+        
+		getline(cin, quit);
     }
     File.close();
 }
@@ -43,16 +92,18 @@ int main()
     cout<<"3. View all contacts: "<<endl;
     cout<<"4. Search for contact: "<<endl;
     cout<<"5. Modify contact: "<<endl;
+    cout<<"6. Encrypt contacts file"<<endl;
+    cout<<"7. Decrypt contacts file"<<endl;
     cin>>c;
     cin.ignore();
 	switch(c)
     {
     	case '1':
-		{
-			addcont();
-			break;
-        }
-        case '2':
+    		{
+    			addcont();
+				break;
+            }
+       /* case '2':
         	{
         		delcont();
         		break;
@@ -71,6 +122,19 @@ int main()
 			{
 				modcont();
 				break
+			}*/
+		case'6':
+			{
+				encdec enc;
+				enc.encrypt();
+				break;
+			}
+		case'7':
+			{
+				encdec dec;
+				dec.decrypt();
+				break;
+				
 			}
 	}
     
